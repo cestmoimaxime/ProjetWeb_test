a2ff91c58c52f33ed434d137415d12b8be144e02
cestmoimaxime <94811875+cestmoimaxime@users.noreply.github.com>
2021-11-25 17:07:58 +0100

    è-do-_dfol

--- index.php
+++ index.php
@@ -9,21 +9,15 @@
   </head>
   <body>
     <div id="header">
-        <h1>Ecran d'accueil<id="topMenu">
+        <h1>L'incroyable jeu :<id="topMenu">
 	<p><img id="image1"  src="titre_harry_potter.jpg"></p>
             <ul>
                 <li class="menuElement"><a href="formulaire.html">START</a></li>
-                <li class="menuElement"><a href="popup.html">popup</a></li>
-                <li class="menuElement"><a href="connect.php">Connexion</a></li>
             </ul>
         </div>
     </div>
-
     <div id="content">
         <p id="texte1">Bienvenu dans cet Escape Game Harry Potter ! Cette page devra évoluer par la suite, mais pour le moment, nous nous contenterons de cette présentation (oups).</p>
-        
-        
     </div>
-  
 </body>
 </html>

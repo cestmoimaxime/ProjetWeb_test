db893c24a073887c4d79261002289541ae5bae51
cestmoimaxime <94811875+cestmoimaxime@users.noreply.github.com>
2021-11-27 21:45:32 +0100

    xftdjrjiiii

--- leaflet.php
+++ leaflet.php
@@ -68,6 +68,26 @@
 
 <?php 
 include('connect.php');
+
+$sql = "SELECT blabla  FROM icone WHERE nom='Hagrid' " ;
+
+$result = mysqli_query($link,$sql);
+
+if (!$result) {
+  echo mysqli_error($link);
+}
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo $row["nom"];
+  }
+}
+else{
+  echo("ca marche pas");
+}
+$link->close();
+
  ?>
     <script src="carto.js"></script>
     <script src="popup.js"></script>
